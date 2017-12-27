032017f2dda0ef003bd23c0e5e12fd5007dbd52c
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2017-12-27 16:28:38 -0600

    allow user to add vehicles. only exit on input EXIT

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
@@ -11,32 +13,25 @@ public class App {
         Vehicle truck = new Vehicle(1999, "Ford", "Ranger", 100000, 4000);
         Vehicle crossover = new Vehicle(1998, "Toyota", "Rav-4", 200000, 3500);
 
-        Vehicle[] allVehicles = {hatchback, suv, sedan, truck, crossover};
+        ArrayList<Vehicle> allVehicles = new ArrayList<Vehicle>();
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("WELCOME TO OUR AWESOME CAR DEALERSHIP! ENTER EITHER: \"ALL VEHICLES\" OR \"SEARCH PRICE\"");
+        allVehicles.add(hatchback);
+        allVehicles.add(suv);
+        allVehicles.add(sedan);
+        allVehicles.add(truck);
+        allVehicles.add(crossover);
 
-        try {
-            String navigationChoice = bufferedReader.readLine();
+        boolean programRunning = true;
 
-            if (navigationChoice.equals("ALL VEHICLES")) {
-                for (Vehicle thingy : allVehicles) {
-                    System.out.println("----------------------");
-                    System.out.println(thingy.year);
-                    System.out.println(thingy.brand);
-                    System.out.println(thingy.model);
-                    System.out.println(thingy.miles);
-                    System.out.println(thingy.price);
-                }
-            } else if (navigationChoice.equals("SEARCH PRICE")) {
-                System.out.println("What is your maximum budget for a vehicular device?");
-                String stringUserMaxBudget = bufferedReader.readLine();
-                int userMaxBudget = Integer.parseInt(stringUserMaxBudget);
+        while (programRunning) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("WELCOME TO OUR AWESOME CAR DEALERSHIP! ENTER EITHER: \"ALL VEHICLES\", \"ADD VEHICLE\" OR \"SEARCH PRICE\". \"EXIT\" WILL EXIT.");
 
-                System.out.println("Awesome! Here are the thingies you can afford: ");
+            try {
+                String navigationChoice = bufferedReader.readLine();
 
-                for (Vehicle thingy : allVehicles) {
-                    if (thingy.worthBuying(userMaxBudget)) {
+                if (navigationChoice.equals("ALL VEHICLES")) {
+                    for (Vehicle thingy : allVehicles) {
                         System.out.println("----------------------");
                         System.out.println(thingy.year);
                         System.out.println(thingy.brand);
@@ -44,13 +39,53 @@ public class App {
                         System.out.println(thingy.miles);
                         System.out.println(thingy.price);
                     }
-                }
-            } else {
-                System.out.println("Stop being stupid");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+                } else if (navigationChoice.equals("SEARCH PRICE")) {
+                    System.out.println("What is your maximum budget for a vehicular device?");
+                    String stringUserMaxBudget = bufferedReader.readLine();
+                    int userMaxBudget = Integer.parseInt(stringUserMaxBudget);
 
+                    System.out.println("Awesome! Here are the thingies you can afford: ");
+
+                    for (Vehicle thingy : allVehicles) {
+                        if (thingy.worthBuying(userMaxBudget)) {
+                            System.out.println("----------------------");
+                            System.out.println(thingy.year);
+                            System.out.println(thingy.brand);
+                            System.out.println(thingy.model);
+                            System.out.println(thingy.miles);
+                            System.out.println(thingy.price);
+                        }
+                    }
+                } else if (navigationChoice.equals("ADD VEHICLE")) {
+                    System.out.println("In which year was your car made?");
+                    int userVehicleYear = Integer.parseInt(bufferedReader.readLine());
+                    System.out.println("What company made said vehicle?");
+                    String userVehicleBrand = bufferedReader.readLine();
+                    System.out.println("What model is this vehicle of yours?");
+                    String userVehicleModel = bufferedReader.readLine();
+                    System.out.println("How many miles has this great vehicle experienced on the road?");
+                    int userVehicleMiles = Integer.parseInt(bufferedReader.readLine());
+                    System.out.println("How much currency should one expect to pay to acquire this vehicle?");
+                    int userVehiclePrice = Integer.parseInt(bufferedReader.readLine());
+                    Vehicle userVehicle = new Vehicle(userVehicleYear, userVehicleBrand, userVehicleModel, userVehicleMiles, userVehiclePrice);
+                    allVehicles.add(userVehicle);
+                    System.out.println("Here are the details for your new vehicle:");
+                    System.out.println("Alright, here's your new vehicle:");
+                    System.out.println("----------------------");
+                    System.out.println(userVehicle.year);
+                    System.out.println(userVehicle.brand);
+                    System.out.println(userVehicle.model);
+                    System.out.println(userVehicle.miles);
+                    System.out.println(userVehicle.price);
+                } else if (navigationChoice.equals("EXIT")) {
+                    System.out.println("Farewell!");
+                    programRunning = false;
+                } else {
+                    System.out.println("Stop being stupid");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
