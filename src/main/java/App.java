e5cd1cc68bc182314426248359730e3cf8b954ef
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2017-12-27 15:51:25 -0600

    refactor using constructor

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,33 +9,12 @@ public class App {
 
         try {
             String stringUserMaxBudget = bufferedReader.readLine();
-            Vehicle hatchback = new Vehicle();
-            hatchback.year = 1994;
-            hatchback.brand = "Subaru";
-            hatchback.model = "Legacy";
-            hatchback.miles = 170000;
-            hatchback.price = 4000;
 
-            Vehicle suv = new Vehicle();
-            suv.year = 2002;
-            suv.brand = "Ford";
-            suv.model = "Explorer";
-            suv.miles = 100000;
-            suv.price = 7000;
-
-            Vehicle sedan = new Vehicle();
-            sedan.year = 2015;
-            sedan.brand = "Toyota";
-            sedan.model = "Camry";
-            sedan.miles = 50000;
-            sedan.price = 30000;
-
-            Vehicle truck = new Vehicle();
-            truck.year = 1999;
-            truck.brand = "Ford";
-            truck.model = "Ranger";
-            truck.miles = 100000;
-            truck.price = 4000;
+            Vehicle hatchback = new Vehicle(1994, "Subaru", "Legacy", 170000, 4000);
+            Vehicle suv = new Vehicle(2002, "Ford", "Explorer", 100000, 7000);
+            Vehicle sedan = new Vehicle(2015, "Toyota", "Camry", 50000, 30000);
+            Vehicle truck = new Vehicle(1999, "Ford", "Ranger", 100000, 4000);
+            Vehicle crossover = new Vehicle(1998, "Toyota", "Rav-4", 200000, 3500);
 
             Vehicle[] allVehicles = {hatchback, suv, sedan, truck};
 
--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -5,6 +5,14 @@ public class Vehicle {
     public int miles;
     public int price;
 
+    public Vehicle(int year, String brand, String model, int miles, int price) {
+        this.year = year;
+        this.brand = brand;
+        this.model = model;
+        this.miles = miles;
+        this.price = price;
+    }
+
         public boolean worthBuying(int maxPrice) {
             return (price < maxPrice);
         }
